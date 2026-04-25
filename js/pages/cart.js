fea2cbb52c231204c21373007e1334d5318577e6
kilothejewel <Cam0197darries@gmail.com>
2026-04-25 23:04:45 +0200

    feat: implement product grid container in index.html and initialize cart page logic

--- js/pages/cart.js
+++ js/pages/cart.js
@@ -1,7 +1,7 @@
 import { initAuthListener } from "../firebase/auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    initAuthListener();
-    console.log("Cart logic loaded!");
-    // Later we will fetch user's cart from Firestore here.
+  initAuthListener();
+  console.log("Cart logic loaded!");
+  // Later we will fetch user's cart from Firestore here.
 });
